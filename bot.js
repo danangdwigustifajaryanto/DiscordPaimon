04a2b13c78a13e0207e60293916adef5bb863609
Argenta <57741344+argentarevealed@users.noreply.github.com>
2022-05-27 01:28:25 +0700

    Add files via upload

--- bot.js
+++ bot.js
@@ -8,21 +8,11 @@ client.on("ready", () => {
 // Dialogue
 client.on("message", msg => {
   if (msg.content === "paimon?") {
-    msg.reply("Aman boss!");
+    const image = new Discord.MessageAttachment("https://s.imgur.com/images/logo-1200-630.jpg?2","img.png");
+    msg.reply({ files : [image] })
   }
 })
 
-client.on("message", msg => {
-  if (msg.content === "Thank you paimon") {
-    msg.reply("urwellcome traveler ^^");
-  }
-})
 
-// Character Build
-client.on("message", msg => {
-  if (msg.content === "p build ayaka") {
-    msg.reply("https://s.imgur.com/images/logo-1200-630.jpg?2");
-  }
-})
 
 client.login(process.env.DISCORD_TOKEN);
