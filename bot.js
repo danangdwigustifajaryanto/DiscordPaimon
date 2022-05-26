c79a66bd7feda0cc1218c4e390dfa8952fab8537
Argenta <57741344+argentarevealed@users.noreply.github.com>
2022-05-27 02:08:23 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on("message", msg => {
 // Character Build
 client.on("message", msg => {
   if (msg.content === "!build miya") {
-    msg.reply("[img]https://i.ibb.co/hLqY1j5/Yoimiya.jpg[/img]");
+    msg.reply("https://i.ibb.co/hLqY1j5/Yoimiya.jpg");
   }
 })
 
