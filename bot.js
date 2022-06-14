84666e34f5283cdcac67af514529ff9b44e1980d
Argenta <57741344+argentarevealed@users.noreply.github.com>
2022-06-14 13:32:59 +0700

    Add files via upload

--- bot.js
+++ bot.js
@@ -568,7 +568,7 @@ client.on("message", msg => {
     const exampleEmbed = new MessageEmbed()
     .setColor('#1F1F1F')
     .setTitle(':musical_note: Paimon Music Bot :musical_note:')
-    .setDescription("***List command***\n> **Info diputar** \n> ***example***: `/info` \n> \n> **Pause**\n> ***contoh***: `/pause`\n> \n> **Play menggunakan link**\n> ***contoh***: `/play song`\n> \n> **Play link playlist**\n> ***contoh***: `/play playlist`\n> \n> **Play menggunakan pencarian**\n> ***contoh***: `/play search`\n> \n> **Tampilkan antrian**\n> ***contoh***: `/queue`\n> \n> **Berhenti dan keluar**\n> ***contoh***: `/quit`\n> \n> **Resume**\n> ***contoh***: `/resume`\n> \n> **Acak antrian**\n> ***contoh***: `/shuffle`\n> \n> **Skip**\n> ***contoh***: `/skip`\n> \n> **Skip ke lagu lain**\n> ***contoh***: `/skip to`")
+    .setDescription("> **Info diputar** \n> ***example***: `/info` \n> \n> **Pause**\n> ***contoh***: `/pause`\n> \n> **Play menggunakan link**\n> ***contoh***: `/play song`\n> \n> **Play link playlist**\n> ***contoh***: `/play playlist`\n> \n> **Play menggunakan pencarian**\n> ***contoh***: `/play search`\n> \n> **Tampilkan antrian**\n> ***contoh***: `/queue`\n> \n> **Berhenti dan keluar**\n> ***contoh***: `/quit`\n> \n> **Resume**\n> ***contoh***: `/resume`\n> \n> **Acak antrian**\n> ***contoh***: `/shuffle`\n> \n> **Skip**\n> ***contoh***: `/skip`\n> \n> **Skip ke lagu lain**\n> ***contoh***: `/skip to`")
     .setThumbnail(msg.author.avatarURL())
     .setTimestamp()
     .setFooter({ text: 'Genshination',
