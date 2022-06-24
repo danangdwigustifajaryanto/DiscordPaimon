d004fd962dac4a0c118a5e2d9c503fe527805d3c
Argenta <57741344+argentarevealed@users.noreply.github.com>
2022-06-25 01:18:41 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,6 @@ const mongoose = require("mongoose");
 const Discord = require("discord.js");
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", ]});
 
-
 const fs = require("fs");
 //Prefix Folder About
 const prefixabout = "!";
